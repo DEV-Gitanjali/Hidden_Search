8e37527a16a3970ae0f4c93514d787878d0d3768
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-01-04 11:05:29 +0530

    this line dfor how to active when click search bar with the help of js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const search=document.querySelector('.search')
 const btn=document.querySelector('.btn')
 const input =Document.querySelector('.input')
+
+
+
+// this line code for when we toggle search bar it will be active
+btn.addEventListener('click',()=>{
+    search.classList.toggle('active')
+    input.focus()
+})
