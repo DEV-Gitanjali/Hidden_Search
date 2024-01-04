02cd044c602bf64001593fb991611a2bef1df97b
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-01-04 11:13:48 +0530

    this code for all about hidden search using html,css & js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const search=document.querySelector('.search')
-const btn=document.querySelector('.btn')
-const input =Document.querySelector('.input')
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
 
+// this line code when we click search bar it will be active response
 
-
-// this line code for when we toggle search bar it will be active
-btn.addEventListener('click',()=>{
+btn.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
 })
